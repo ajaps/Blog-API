ebcad903c7157397d7210b3e426741e35f207eb0
Framky007 <ajapsfranklin@gmail.com>
2018-10-29 09:47:23 +0100

    Allow user to update information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+ before_action :authenticate_request
+
   attr_reader :current_user
 
   include ExceptionHandler
@@ -7,6 +9,6 @@ class ApplicationController < ActionController::API
   private
   def authenticate_request
     @current_user = AuthorizeApiRequest.call(request.headers).result
-    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+    render json: { error: 'Not Authorized', message: "Invalid/missing token Sign-in or set the token under 'Authorization' in the header" }, status: 401 unless @current_user
   end
 end
