0d86cf88b322d70c6f873ea9bbebb2b49a03a953
Framky007 <ajapsfranklin@gmail.com>
2018-11-23 00:04:31 +0100

    fix home route

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,7 @@
 class IndexController < ApplicationController
+
+  skip_before_action :authenticate_request!, only:  :index
+
   def index
     render json: { messgae: 'Welcome to Blog API' }
   end
